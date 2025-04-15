c507a90b773914b32ae0ad63faf7d9091c40aa86
KassidyJB <jeanbartkassidy51@gmail.com>
2025-04-15 21:49:56 +0000

    changes to html

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,38 @@
 <script>
-
-  
+  import viteLogo from '/vite.svg';
 </script>
 
 
 
+
+
 <main>
-
-<h1> Mars Rover Project </h1>
-
-
-  <div class="card">
+  <h1>Welcome to the Mars Rover Project!</h1>
+  <div>
   <p>+About<p>
   <p>+Rovers<p>
   <p>+Photos<p>
   </div>
 
-
-
+  <p class="read-the-docs">
+  Click one of the tab options above for more!
+  </p>
 </main>
 
 <style>
-  
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
 </style>
