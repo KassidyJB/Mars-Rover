a0c6c97b809ed4bd7140c32c2c30e369b300ccde
KassidyJB <jeanbartkassidy51@gmail.com>
2025-04-15 20:07:10 +0000

    testing out html and css

--- src/App.svelte
+++ src/App.svelte
@@ -3,16 +3,12 @@
   
 </script>
 
+
+
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelte} class="logo" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Mars Rover Project<h1>
+
+<h1> Mars Rover Project </h1>
+
 
   <div class="card">
   <p>+About<p>
@@ -21,25 +17,9 @@
   </div>
 
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  
 </style>
