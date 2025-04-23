6a9323ed0dab61179ef5081feb16bf5108afa9e3
KassidyJB <jeanbartkassidy51@gmail.com>
2025-04-23 22:47:18 +0000

    revamp of stuff. Files being removed to fit the page and layouts needed

---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+  import viteLogo from '/vite.svg';
+</script>
+
+
+<main>
+  <h1>Welcome to the Mars Rover Project!</h1>
+  <div>
+  <p>+About  /  +Rovers  /   +Photos</p>
+  </div>
+
+  <p class="read-the-docs">
+  Click one of the tab options above for more!
+  </p>
+</main>
+
+<style>
+  .read-the-docs {
+    color: #700;
+  }
+</style>
+
+
+App.svelte Info page that I deleted
+
+<script>
+  import viteLogo from '/vite.svg';
+</script>
+
+
+
+
+
+<main>
+  <h1>Welcome to the Mars Rover Project!</h1>
+  <div>
+  <p><a href= "/About">+About</a>  /  <a href= "/Rovers">+Photo Search</a>  /   +Learn More</p>
+  </div>
+
+  <p class="read-the-docs">
+  Click one of the tab options above for more!
+  </p>
+</main>
+
